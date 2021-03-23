6e0dd373c045ce7bcc8aa2c61d42b4e60c812c61
Feng Xiang <fxiang@seas.upenn.edu>
2021-03-23 03:54:27 -0400

    add login unit-test, add props mocking

--- client/src/tests/register.test.js
+++ client/src/tests/register.test.js
@@ -24,4 +24,25 @@ describe('webpage correctly handles submit & shows msg from server', () => {
       expect(screen.getByText('✖ Registration failed: Invalid username or password provided.')).toBeInTheDocument();
     });
   });
+
+  test('mock redirect is called when registration was successful', async () => {
+    const mockPush = jest.fn().mockImplementation(() => {});
+    const mockRouterProps = { push: mockPush };
+    fetch.mockResponseOnce(JSON.stringify({
+      status: 'ok',
+      msg: 'Registration was successfull.',
+    }));
+
+    const { getByRole } = render(<Register.WrappedComponent history={mockRouterProps}/>);
+    fireEvent.click(getByRole('button', { name: 'Register' }));
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith({
+        pathname: '/login',
+        state: {
+          msg: '✔ Registration was successful. Please login using your crendentials.',
+        },
+      });
+    });
+  });
 });
